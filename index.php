04c071899df0f5bb7816062c828c830e9480b3ef
unknown <aluno@.(none)>
2010-03-11 20:44:44 -0300

    primeiro commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8">
+	<title>Hello World</title>
+	<!--[if IE]>
+  	<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	<style>
+	  article, aside, figure, footer, header, hgroup,
+	  menu, nav, section { display: block; }
+	</style>
+	
+</head>
+<body>
+	<div class="container">
+	<?php echo '<h1>Hello World do PHP</h1>'?>
+	<p>Copyritght &copy; <?php echo date('Y')  ?> </p>
+	</div>
+</body>
+</html>
